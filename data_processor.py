ccb697f5a65fafafa01a0e51c9e4af70bc4c01da
Daniel Kiedrowski <daniel@levire.com>
2017-10-29 01:02:59 +0200

    ADDED hdf5 store for data pre processor.

--- data_processor.py
+++ data_processor.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from datetime import datetime
 
-INPUT_FILE = "./data/train_100.csv"
-OUTPUT_FILE = "./processed_data/data_100.csv"
+INPUT_FILE = "./data/train_10000.csv"
+OUTPUT_FILE = "./processed_data/data_10000.h5"
 
 if __name__ == "__main__":
     data = pd.read_csv(
@@ -12,7 +12,25 @@ if __name__ == "__main__":
             "date": (lambda d: datetime.strptime(d, "%Y-%M-%d").weekday())
         },
         keep_default_na=False,
-        index_col=0
+        index_col=0,
+        engine="c"
     )
 
-    data.to_csv(OUTPUT_FILE)
+    data.to_hdf(OUTPUT_FILE, "data", mode="w", format="table")
+    del data
+
+    hdf = pd.HDFStore(OUTPUT_FILE)
+    id = hdf.select("data", where="columns = ['id']")
+    day = pd.get_dummies(hdf.select("data", where="columns = ['date']"), columns=["date"])
+    item = pd.get_dummies(hdf.select("data", where="columns = ['item_nbr']"), columns=["item_nbr"])
+    store = pd.get_dummies(hdf.select("data", where="columns = ['store_nbr']"), columns=["store_nbr"])
+    promotion = hdf.select("data", where="columns = ['onpromotion']")
+    sales = hdf.select("data", where="columns = ['unit_sales']")
+    hdf.close()
+
+    del hdf
+
+    normalized_data = pd.concat([id, day, store, item, promotion, sales], axis=1)
+    normalized_data.to_hdf(OUTPUT_FILE, "data", mode="w", format="table")
+
+    print(normalized_data.head())
