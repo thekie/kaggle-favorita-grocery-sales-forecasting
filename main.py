a1ca69dc39c84917ad359f9e658244d8f6cdc276
Daniel Kiedrowski <daniel@levire.com>
2017-10-28 20:39:30 +0200

    Using Keras and a a NN to predict the prices per item in a store for a given weekday.

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
-import tensorflow as tf
-import numpy as np
+import pandas as pd
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.optimizers import SGD
 
-TRAININGS_DATA_FILE = "./processed_data/data_10000.csv"
+TRAININGS_DATA_FILE = "./processed_data/data_100.csv"
 
 if __name__ == "__main__":
-    trainings_data = tf.contrib.learn.datasets.base.load_csv_without_header(
-        filename=TRAININGS_DATA_FILE,
-        target_column=3,
-        target_dtype=np.float32,
-        features_dtype=np.float32
-    )
-    trainings_np_data = np.array(trainings_data.data)[:, 1:]
-
-    feature_columns = [tf.feature_column.numeric_column("x", shape=(4,))]
-    optimizer = tf.train.GradientDescentOptimizer(0.01)
-    estimator = tf.estimator.LinearRegressor(feature_columns=feature_columns)
-
-    train_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": trainings_np_data},
-        y=np.array(trainings_data.target),
-        num_epochs=10000,
-        shuffle=True
+    data = pd.read_csv(
+        TRAININGS_DATA_FILE,
+        usecols=["id", "date", "store_nbr", "item_nbr", "onpromotion", "unit_sales"]
     )
 
-    estimator.train(train_input_fn)
-    print(estimator.evaluate(train_input_fn))
+    print(data.head())
+    day = pd.get_dummies(data["date"])
+    item = pd.get_dummies(data["item_nbr"])
+    store = pd.get_dummies(data["store_nbr"])
+    promotion = data["onpromotion"]
+    sales = data["unit_sales"]
 
-    new_sample = np.array([[1, 25, 7.0, 0]], dtype=np.float32)
-    predict_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": new_sample},
-        num_epochs=1,
-        shuffle=False
+    normalized_data = pd.concat([day, store, item, promotion], axis=1)
+    print(normalized_data.head())
+
+    model = Sequential([
+        Dense(128, input_dim=normalized_data.shape[1], activation="sigmoid"),
+        Dense(1, activation="linear")
+    ])
+
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss="mse", metrics=["accuracy"],)
+
+    model.fit(x=normalized_data.values, y=sales.values, epochs=1000, batch_size=100)
+
+    prediction = model.predict(normalized_data.values)
+
+    prediction_data = pd.DataFrame(
+        data=prediction,
+        columns=["unit_sales"]
     )
 
-    prediction = estimator.predict(predict_input_fn)
-    print([p for p in prediction])
+    submission_data = pd.concat([data["id"], prediction_data], axis=1)
+    submission_data.to_csv("submission.csv", index=False)
